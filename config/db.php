1e5b6bf19285f288bade182a1e0c9eb0e3e0a42e
keks13 <moaner48@gmail.com>
2018-06-02 22:27:05 +0300

    added gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=techus.com',
-    'username' => 'vanya',
-    'password' => 'vanya',
-    'charset' => 'utf8',
-];
