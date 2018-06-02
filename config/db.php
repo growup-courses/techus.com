3702a800ca1f6955594569be2224a5c46be908d2
Dzemlianoi <dzemlianoi@gmail.com>
2018-06-02 16:15:19 +0300

    base changed

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=techus',
-    'username' => 'denis',
+    'username' => 'root',
     'password' => 'denis',
     'charset' => 'utf8',
 ];
