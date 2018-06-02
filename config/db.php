2f05b4e433356910c2f0295be53fc2357d507530
Dzemlianoi <dzemlianoi@gmail.com>
2018-06-02 15:54:07 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=techus',
+    'username' => 'denis',
+    'password' => 'denis',
+    'charset' => 'utf8',
+];
