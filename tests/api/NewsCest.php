50d0379a6363e09979f35e0b90dedaad73e115a0
keks13 <moaner48@gmail.com>
2018-06-08 11:22:34 +0300

    fix .gitignore NewsCest

--- tests/api/NewsCest.php
+++ tests/api/NewsCest.php
@@ -1,47 +1,48 @@
 <?php
+
 use app\models\News;
 use Faker\Factory;
 
 class NewsCest {
-public function CreateNews()
-{
-  $faker = Factory::create();
-  $news = new News();
-  $news -> author =  $faker-> firstName;
-  $news -> text = $faker -> text;
-  $news -> likes = $faker -> randomDigit;
-  $news -> dislikes = $faker -> randomDigit;
-  $news -> save();
-  return $news;
-}
-  public function AddLikes(ApiTester $I)
-  {
-    $news = $this->CreateNews();
-    $I -> sendGET('/news/add-like/' . $news->id);
+
+public function CreateNews() {
+    $faker = Factory::create();
+    $news = new News();
+    $news -> author =  $faker-> firstName;
+    $news -> text = $faker -> text;
+    $news -> likes = $faker -> randomDigit;
+    $news -> dislikes = $faker -> randomDigit;
+    $news -> save();
+    return $news;
+  }
+
+  public function AddLikes(ApiTester $I) {
+    $news = $this -> CreateNews();
+    $I -> sendGET('/news/add-like/' . $news -> id);
     $I -> seeResponseCodeIs(200);
     $I -> seeResponseIsJson();
     $I -> seeResponseContainsJson([
-      'likes' => $news->likes +1,
+      'id' => $news -> id,
+      'likes' => $news -> likes +1
     ]);
     News::deleteAll();
   }
 
-  public function AddDislikes(ApiTester $I)
-  {
-    $news = $this->CreateNews();
-    $I -> sendGET('/news/add-dislike/' . $news->id);
+  public function AddDislikes(ApiTester $I) {
+    $news = $this -> CreateNews();
+    $I -> sendGET('/news/add-dislike/' . $news -> id);
     $I -> seeResponseCodeIs(200);
     $I -> seeResponseIsJson();
     $I -> seeResponseContainsJson([
-      'dislikes' => $news->dislikes +1,
+      'id' => $news -> id,
+      'dislikes' => $news -> dislikes +1,
     ]);
     News::deleteAll();
   }
 
-  public function Show(ApiTester $I)
-  {
-    $news = $this->CreateNews();
-    $I -> sendGET('/news/' . $news->id);
+  public function Show(ApiTester $I) {
+    $news = $this -> CreateNews();
+    $I -> sendGET('/news/' . $news -> id);
     $I -> seeResponseCodeIs(200);
     $I -> seeResponseIsJson();
     $I -> seeResponseContainsJson([
@@ -54,9 +55,8 @@ public function CreateNews()
     News::deleteAll();
   }
 
-  public function Index(ApiTester $I)
-  {
-    $news = $this->CreateNews();
+  public function Index(ApiTester $I) {
+    $news = $this -> CreateNews();
     $I -> sendGET('/news');
     $I -> seeResponseCodeIs(200);
     $I -> seeResponseIsJson();
