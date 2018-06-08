2edc6e5e8542ee9e71277ab480e671baaff9444a
Dzemlianoi <dzemlianoi@gmail.com>
2018-06-08 21:15:01 +0300

    updated circle

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=techus_test';
+$db['dsn'] = 'mysql:host=127.0.0.1;dbname=techus_test';
 
 return $db;
