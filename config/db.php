e4f4ba17168d7441f809785de7c70d3f301c14ce
keks13 <moaner48@gmail.com>
2018-06-08 23:58:26 +0300

    added .circleci

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=techus',
+    'dsn' => 'mysql:host=localhost;dbname=techus_test',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
